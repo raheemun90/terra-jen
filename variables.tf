fa082123f364bbb226a0fe6cd6536d9df5d504f1
raheemun90 <raheemun90@gmail.com>
2026-05-18 13:57:39 +0300

    variables.tf

--- variables.tf
+++ variables.tf
@@ -5,3 +5,14 @@ variable "instance_type" {
 variable "key_name" {
   default = "jen"
 }
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  default     = ""
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+  default     = ""
+}
